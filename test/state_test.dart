81c423a3276bdf5feefa787e369c846758a4eeea
h.ayabe <h.ayabe_biz@live.jp>
2020-12-20 15:17:52 +0900

    fix test

--- test/state_test.dart
+++ test/state_test.dart
@@ -3,9 +3,10 @@ import 'package:test/test.dart';
 
 // 理想のコードであり、動きません!!
 // notifierでWidgetを返すメソッドで定義できた場合のテストコード
+// aya-eiya) 以下ではだめですか？
 void main() {
   test('my first unit test', () {
     var notifier = AppStateNotifier();
-    expect(notifier.up().state.countState, 1);
+    expect((notifier..up()).state.countState, 1);
   });
 }
